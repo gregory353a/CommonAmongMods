660635f702e55dce76c7d4485d72451159115503
Grzegorz Pająk <gregory353a@gmail.com>
2021-04-28 20:54:07 +0200

    Creation desktop directory

--- setup.cmd
+++ setup.cmd
@@ -1,4 +1,19 @@
 @ECHO OFF
 
 set sh=./GIT/bin/sh.exe
+set shfp="%CD%/GIT/bin/sh.exe"
 sh -c "scripts/updateMods"
+
+set amongdir=%CD%
+set desktopdir=%USERPROFILE%/Desktop
+
+mkdir "%desktopdir%/Among Us Mody"
+
+echo %shfp% -c "%amongdir%\scripts\ChangeMod" master >> "%desktopdir%\Among Us Mody\Among Vanila.cmd"
+echo %shfp% -c "%amongdir%\Among Us.exe" >> "%desktopdir%\Among Us Mody\Among Vanila.cmd" 
+
+echo %shfp% -c "%amongdir%\scripts\ChangeMod" tor >> "%desktopdir%\Among Us Mody\The other roles.cmd"
+echo %shfp% -c "%amongdir%\Among Us.exe" >> "%desktopdir%\Among Us Mody\The other roles.cmd" 
+
+echo %shfp% -c "%amongdir%\scripts\ChangeMod" tou >> "%desktopdir%\Among Us Mody\Town of us.cmd"
+echo %shfp% -c "%amongdir%\Among Us.exe" >> "%desktopdir%\Among Us Mody\Town of us.cmd"
